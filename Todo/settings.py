b24c59b85a773e9a924d766cc8d961c75642bcc7
Harsh Jain <f20190211@pilani.bits-pilani.ac.in>
2023-07-30 20:27:10 +0530

    deploy

--- Todo/settings.py
+++ Todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-112kng)e@j*+7z^axe=8pd##bj-tz0m*n)=n3tq@ef!%q9z^98
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-production-907a.up.railway.app']
+ALLOWED_HOSTS = ['*', 'todo-production-907a.up.railway.app']
 
 
 # Application definition
