c88f9c1b3853c4b887161a7dc335d46325908b42
harshjain0702 <88145247+harshjain0702@users.noreply.github.com>
2023-07-31 01:02:20 +0530

    Update urls.py

--- application/urls.py
+++ application/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, Register
 from django.contrib.auth.views import LogoutView
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('register/', Register.as_view(), name='register'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-    path('create-task/', TaskCreate.as_view(), name='task-create'),
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
-    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+    path('login/', csrf_exempt(CustomLoginView.as_view()), name='login'),
+    path('register/', csrf_exempt(Register.as_view()), name='register'),
+    path('logout/', csrf_exempt(LogoutView.as_view(next_page='login')), name='logout'),
+    path('', csrf_exempt(TaskList.as_view()), name='tasks'),
+    path('task/<int:pk>/', csrf_exempt(TaskDetail.as_view()), name='task'),
+    path('create-task/', csrf_exempt(TaskCreate.as_view()), name='task-create'),
+    path('task-update/<int:pk>/', csrf_exempt(TaskUpdate.as_view()), name='task-update'),
+    path('task-delete/<int:pk>/', csrf_exempt(TaskDelete.as_view()), name='task-delete'),
 ]
