bffdbbab9aa2f8cf81beb01f1a00ec3e55d77132
harshjain0702 <88145247+harshjain0702@users.noreply.github.com>
2023-07-30 21:19:13 +0530

    Update settings.py

--- Todo/settings.py
+++ Todo/settings.py
@@ -83,6 +83,12 @@ DATABASES = {
     }
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
